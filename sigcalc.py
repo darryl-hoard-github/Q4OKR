434f0f80bbff9373bf049b820681af1a1866aeeb
work-and-play <34891729+work-and-play@users.noreply.github.com>
2018-04-09 01:03:55 -0400

    Update sigcalc.py

--- sigcalc.py
+++ sigcalc.py
@@ -1,25 +1,3 @@
-from hashlib import sha1 
-import hmac
-import time 
-
-try:
-    # Python 3
-    from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
-    import sys
-    def test (*args):
-        test_orig(*args, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
-except ImportError: # Python 2
-    from BaseHTTPServer import HTTPServer, test
-    from SimpleHTTPServer import SimpleHTTPRequestHandler
-
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
-
-if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer)
-
 def sign_request(nonce):
 	api_id = "31d4d730-1aad-0136-9a37-0abfe2d6609e"
 	time_stamp = str(time.time())
