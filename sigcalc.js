2303d9b2532bf124d0232cb845886121b621a981
work-and-play <34891729+work-and-play@users.noreply.github.com>
2018-04-09 20:40:48 -0400

    Create sigcalc.js

---
+++ sigcalc.js
@@ -0,0 +1,13 @@
+jsSHA = require("jssha");
+var api_id = "31d4d730-1aad-0136-9a37-0abfe2d6609e";
+var secret = "zq9EcsMxT2Y4tQwU9hQVJ3GIkNuAUnaF1XQWcJcFY";
+var timestamp = Date.now();
+var data = "";
+var nonce = "OetR6gkPEu";
+var shaObj = new jsSHA("SHA-1", "TEXT");
+shaObj.setHMACKey(secret, "TEXT");
+shaObj.update(api_id+timestamp+nonce+data);
+var hmac = shaObj.getHMAC("HEX");
+$('#signature').val(str(hmac));
+$('#nonce').val(str(nonce))
+$('#timestamp').val(str(timestamp))
